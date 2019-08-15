dfd8c0be0fa678154ef7618d9518bd81015a29b5
czh <1419507781@qq.com>
2019-08-15 16:24:48 +0800

    tab-bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
     <h1>小橘子项目启动仪式</h1>
+    <TabBar></TabBar>
   </div>
 </template>
+<script>
+import TabBar from './components/tab-bar/tab-bar'
+export default {
+    components:{
+      TabBar,
+    }
+}
+</script>
 
 <style lang="scss">
 
